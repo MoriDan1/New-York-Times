9600c0e75010798ff00a32836f7478ac51c43d54
MoriDan1 <emanuelecirillo004@gmail.com>
2022-12-13 18:56:13 +0100

    account

---
+++ pagina1/script.js
@@ -0,0 +1,4 @@
+  let item = document.querySelector('.icon-hamburger');
+  item.addEventListener("click", function() {
+        document.body.classList.toggle('menu-open');
+  });
--- script.js
+++
@@ -1,4 +0,0 @@
-let item = document.querySelector('.icon-hamburger');
-item.addEventListener("click", function() {
-      document.body.classList.toggle('menu-open');
-});
